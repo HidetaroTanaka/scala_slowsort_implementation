aa785bcdc2ede9d7af09e2d63557dbbe16ee4bd6
Hidetaro Tanaka <htanaka@www.nj.cs.tuat.ac.jp>
2022-08-05 13:56:55 +0900

    fix

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,26 @@
-import scala.math.Ordered.orderingToOrdered
+import scala.math._
 import scala.util.Random
 
 object Main extends App {
-  def slowsort[T: Ordering](l: List[T]): List[T] = {
+  def slowsort[T: Ordering](l: List[T], f: (T, T) => Boolean): List[T] = {
     if(l.length <= 1) l
     else {
-      val sorted_first_half = slowsort(l.slice(0,l.length/2))
-      val sorted_second_half = slowsort(l.slice(l.length/2, l.length))
-      if(sorted_first_half.head < sorted_second_half.head) {
-        sorted_first_half.head :: slowsort(sorted_first_half.tail ::: sorted_second_half)
+      val sorted_first_half = slowsort(l.slice(0,l.length/2), f)
+      val sorted_second_half = slowsort(l.slice(l.length/2,l.length), f)
+      if(f(sorted_first_half.head, sorted_second_half.head)) {
+        sorted_first_half.head :: slowsort(sorted_first_half.tail ::: sorted_second_half, f)
       } else {
-        sorted_second_half.head :: slowsort(sorted_first_half ::: sorted_second_half.tail)
+        sorted_second_half.head :: slowsort(sorted_first_half ::: sorted_second_half.tail, f)
       }
     }
   }
+
   Random.setSeed(0)
   val array_before_sort = Random.shuffle((0 until 32).toList)
-  println("Array before sort:")
+  println("ソート前のリスト:")
   println(array_before_sort.mkString("(",", ",")"))
-  println("Array after sort:")
-  println(slowsort(array_before_sort).mkString("(",", ",")"))
+  println("昇順ソート:")
+  println(slowsort[Int](array_before_sort, _ < _).mkString("(",", ",")"))
+  println("降順ソート:")
+  println(slowsort[Int](array_before_sort, _ > _).mkString("(",", ",")"))
 }
